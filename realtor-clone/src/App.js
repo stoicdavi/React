e423020f7266196b93578ac03f85181a3f004bc7
David Nanjila <nanjiladavid2@gmail.com>
2025-01-04 11:15:21 +0300

    set up my project
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

---
+++ realtor-clone/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-green-600 bg-red-100'> React App</h1>
+      <p className="text-blue-500 font-serif ">Hello David you are doing well</p>
+    </>
+  );
+}
+
+export default App;
